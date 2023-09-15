4f6bb708e2e0c54aa238e65d53bb0016ba74e8d7
Houseofreasoning <houseofreasoning@gmail.com>
2023-09-15 17:00:47 +0100

    mr j

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function james(){
+    alert('Welcome to JS class')
+}
+
+function jam(){
+    document.getElementById('demo').innerHTML='No it is js class'; 
+    document.getElementById('demo').style='color:blue';
+}
+
+function cal (){
+    let a = 5;
+    let b = 6;
+    console.log(a+b);
+}
+
+function tai(){
+    let a = 'how old are you'
+    prompt(a);
+}
+
+function man(){
+    confirm('Do you want to close the page?');
+}
+
+let k=prompt('Visitor welcome What is your name?');
+let l=`Welcome ${k}`;
+alert(l)
